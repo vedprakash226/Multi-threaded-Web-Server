f86c4ac1dd603f467392a308757d02b5ce8ecf69
Ved Prakash Vishwakarma <vedprakash22@iitk.ac.in>
2025-12-31 16:02:10 +0530

    Handle request and cache find function implemented

--- server_LRU_cache.cpp
+++ server_LRU_cache.cpp
@@ -1,17 +1,23 @@
 #include "proxy_parse.h"
-#include <bits/stdc++.h>
-#include <pthread.h>
-#include<semaphore.h>
+#include<bits/stdc++.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include<fcntl.h>
-#include<sys/wait.h>
-#include<errno.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
 
-#define MAX_CLIENTS 20          //one client per thread
-#define MAX_BYTES 10240
+#define MAX_BYTES 4096    //max allowed size of request/response
+#define MAX_CLIENTS 400     //max number of client requests served at a time
+#define MAX_SIZE 200*(1<<20)     //size of the cache
+#define MAX_ELEMENT_SIZE 10*(1<<20)     //max size of an element in cache
 
 struct cacheElement{
     char* data;
@@ -21,9 +27,44 @@ struct cacheElement{
     cacheElement* next;
 };
 
-cacheElement* find(char* url);
-int add_cacheElement(char* url, char* data, int length);
+// LRU cache functions
+cacheElement* find(char* url){
+    cacheElement* temp = nullptr;
+    pthread_mutex_lock(&lock);  //locking the shared resource
+    std::cout<<"Locked the cache for lookup"<<std::endl;
+
+    if(head!=nullptr){
+        cacheElement* temp = head;
+        while(temp!=nullptr){
+            if(!strcmp(temp->url, url)){
+                std::cout<<"Cache hit for URL: "<<std::endl;
+                
+                temp->timestamp_cache = time(nullptr);   //update the timestamp as latest
+                break;
+            }
+            temp = temp->next;
+        }
+    }else{
+        std::cout<<"Cache miss for URL"<<std::endl;
+    }
+
+    pthread_mutex_unlock(&lock);    //unlocking the shared resource
+    std::cout<<"Unlocked the cache after lookup"<<std::endl;
+    return temp;
+}
+
+// function to add element to cache
+int add_cacheElement(char* url, char* data, int length){
+    //aquire the lock
+    pthread_mutex_lock(&lock);
+    std::cout<<"Locked the cache for addition"<<std::endl;
+
+    
+}
+
 void remove_cacheElement();
+
+
 
 int portNumber;          
 int proxyServerSocketID;
@@ -35,6 +76,157 @@ sem_t semaphore;   // semaphore to limit number of clients
 // LRU cache variables head
 cacheElement* head;
 int cacheSize;
+
+// check if the HTTP version is supported
+int checkHTTPversion(char *msg){
+	if(!strncmp(msg, "HTTP/1.1", 8)) return 1;
+	else if(!strncmp(msg, "HTTP/1.0", 8)) return 1;
+	
+    return -1;
+}
+
+//sending error message function
+int sendErrorMessage(int socket, int status_code)
+{
+	char str[1024];
+	char currentTime[50];
+	time_t now = time(0);
+
+	struct tm data = *gmtime(&now);
+	strftime(currentTime,sizeof(currentTime),"%a, %d %b %Y %H:%M:%S %Z", &data);
+
+	switch(status_code)
+	{
+		case 400: snprintf(str, sizeof(str), "HTTP/1.1 400 Bad Request\r\nContent-Length: 95\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>400 Bad Request</TITLE></HEAD>\n<BODY><H1>400 Bad Rqeuest</H1>\n</BODY></HTML>", currentTime);
+				  printf("400 Bad Request\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 403: snprintf(str, sizeof(str), "HTTP/1.1 403 Forbidden\r\nContent-Length: 112\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>403 Forbidden</TITLE></HEAD>\n<BODY><H1>403 Forbidden</H1><br>Permission Denied\n</BODY></HTML>", currentTime);
+				  printf("403 Forbidden\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 404: snprintf(str, sizeof(str), "HTTP/1.1 404 Not Found\r\nContent-Length: 91\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n<BODY><H1>404 Not Found</H1>\n</BODY></HTML>", currentTime);
+				  printf("404 Not Found\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 500: snprintf(str, sizeof(str), "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 115\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>500 Internal Server Error</TITLE></HEAD>\n<BODY><H1>500 Internal Server Error</H1>\n</BODY></HTML>", currentTime);
+				  //printf("500 Internal Server Error\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 501: snprintf(str, sizeof(str), "HTTP/1.1 501 Not Implemented\r\nContent-Length: 103\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Implemented</TITLE></HEAD>\n<BODY><H1>501 Not Implemented</H1>\n</BODY></HTML>", currentTime);
+				  printf("501 Not Implemented\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 505: snprintf(str, sizeof(str), "HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 125\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>505 HTTP Version Not Supported</TITLE></HEAD>\n<BODY><H1>505 HTTP Version Not Supported</H1>\n</BODY></HTML>", currentTime);
+				  printf("505 HTTP Version Not Supported\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		default:  return -1;
+
+	}
+	return 1;
+}
+
+int connectRemoteServer(char* hostAddress, size_t port_num){
+    int remoteSocketId = socket(AF_INET, SOCK_STREAM, 0);
+    if(remoteSocketId<0){
+        std::cerr<<"Remote server socket creation failed"<<std::endl;
+        return -1;
+    }
+
+    //resolve the host address
+    hostent* serverHost = gethostbyname(hostAddress);
+    if(serverHost==nullptr){
+        std::cerr<<"Failed to resolve host: "<<hostAddress<<std::endl;
+        return -1;
+    }
+
+    // filling the sercer address structure
+    sockaddr_in serverAddress = {};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port= htons(port_num);
+    std::memcpy(&serverAddress.sin_addr.s_addr, serverHost->h_addr, serverHost->h_length);
+
+    if(connect(remoteSocketId, (sockaddr*)&serverAddress, sizeof(serverAddress))<0){
+        std::cerr<<"Connection to remote server failed"<<std::endl;
+        return -1;
+    }
+
+    return remoteSocketId;
+}
+
+// client handler
+int handleRequest(int clientSocketID, ParsedRequest* request, std::string &tempReq){
+    std::vector<char> buffer(MAX_BYTES, '\0');
+    strcpy(buffer.data(), "GET");
+    strcat(buffer.data(), request->path);
+    strcat(buffer.data(), " ");
+    strcat(buffer.data(), request->version);
+
+    size_t bufferLength = strlen(buffer.data());
+
+    if(ParsedHeader_set(request, "Connection", "close")<0){
+        std::cerr<<"Failed to set Connection header"<<std::endl;
+    }
+
+    if(ParsedHeader_get(request, "Host")==nullptr){
+        if(ParsedHeader_set(request, "Host", request->host)<0){
+            std::cerr<<"Failed to set Host header"<<std::endl;
+        }
+    }
+
+    if(ParsedRequest_unparse_headers(request, buffer.data()+bufferLength, (size_t)(MAX_BYTES-bufferLength))<0){
+        std::cerr<<"Failed to unparse headers"<<std::endl;
+    }
+
+    size_t serverPort = 80;
+    if(request->port){
+        serverPort = atoi(request->port);
+    }
+
+    int remoteSocketID = connectRemoteServer(request->host, serverPort);
+    if(remoteSocketID<0) return -1;
+
+    int bytesSent = send(remoteSocketID, buffer.data(), strlen(buffer.data()), 0);
+    fill(buffer.begin(), buffer.end(), '\0');
+
+    bytesSent= recv(remoteSocketID, buffer.data(), MAX_BYTES-1, 0);
+
+    //temporary buffer to store response
+    std::vector<char> tempResponse;
+    tempResponse.reserve(MAX_BYTES);
+
+    while(bytesSent>0){
+        //send to client
+        send(clientSocketID, buffer.data(), bytesSent, 0);
+
+        //store in temporary response buffer
+        for(int i{0};i<bytesSent;i++){
+            tempResponse.push_back(buffer[i]);
+        }
+
+        if(bytesSent<0){
+            std::cerr<<"Error in receiving data from remote server"<<std::endl;
+            break;
+        }
+
+        std::fill(buffer.begin(), buffer.end(), '\0');
+        bytesSent= recv(remoteSocketID, buffer.data(), MAX_BYTES-1, 0);
+    }
+
+    //null termainate tempResponse
+    tempResponse.push_back('\0');
+
+    add_cacheElement(tempResponse.data(), tempReq.data(), strlen(tempReq.data()));
+    close(remoteSocketID);
+    return 0;
+}
 
 // Thread function to handle each client request
 void* threadFunc(void* newSocket){
@@ -57,10 +249,10 @@ void* threadFunc(void* newSocket){
 
     while(bytesReceived>0){
         //find how much buffer is used
-        dataLength = std::strlen(buffer.data());
+        dataLength = strlen(buffer.data());
 
         //find if the request ended?
-        if(std::strstr(buffer.data(), "\r\n\r\n")==nullptr){
+        if(strstr(buffer.data(), "\r\n\r\n")==nullptr){
             bytesReceived = recv(socketID, buffer.data()+dataLength, MAX_BYTES - dataLength, 0);
         }else{
             break;
@@ -68,7 +260,7 @@ void* threadFunc(void* newSocket){
     }
 
     //make a copy of the buffer to find it in cache
-    std::string tempReq(buffer.data(), std::strlen(buffer.data()));
+    std::string tempReq(buffer.data(), strlen(buffer.data()));
 
     cacheElement* temp = find(tempReq.data());      //lookup in the cache if it exits
     if(temp!=nullptr){
@@ -87,7 +279,48 @@ void* threadFunc(void* newSocket){
         // found the data in the cache
         std::cout<<"Data found in cache. Served from cache."<<std::endl;
     }
+    else if(bytesReceived>0){
+        //data not found in cache
+        //parse the reques
+        ParsedRequest *request = ParsedRequest_create();
 
+        if(ParsedRequest_parse(request, buffer.data(), dataLength)<0){
+            std::cerr<<"Request parsing failed"<<std::endl;
+        }else{
+            std::fill(buffer.begin(), buffer.end(), '\0');
+
+            if(!strcmp(request->method, "GET")){
+                if(request->host and request->path and checkHTTPversion(request->version)==1){
+                    bytesReceived = handleRequest(socketID, request, tempReq);
+
+                    // if no response from the server
+                    if(bytesReceived==-1){
+                        sendErrorMessage(socketID, 500);
+                    }
+                }else{
+                    sendErrorMessage(socketID, 500);
+                }
+            }else{
+                std::cerr<<"Unsupported HTTP method received: "<<request->method<<std::endl;
+            }
+        }
+        
+        ParsedRequest_destroy(request);
+    }
+    else if(bytesReceived==0){
+        std::cerr<<"Client disconnected unexpectedly."<<std::endl;
+    }
+    else{
+        std::cerr<<"Error in receiving data from client."<<std::endl;
+    }
+
+    shutdown(socketID, SHUT_RDWR);
+    close(socketID);
+    sem_post(&semaphore);    //it increases the value of semaphore by 1
+    sem_getvalue(&semaphore, &currentSemaphoreVal);
+    std::cout<<"Connection closed. Current semaphore value: "<<currentSemaphoreVal<<std::endl;
+
+    return nullptr;
 
 }
 
